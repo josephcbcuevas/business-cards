ce210f645ed711513d1befab69b758ab0e2fa99b
bundlarjoe <joe.cuevas@bundlar.com>
2023-02-09 12:33:12 -0600

    Added Headshot with styles

--- src/HeaderDark.js
+++ src/HeaderDark.js
@@ -3,7 +3,7 @@ import SocialDark from "./SocialDark.js";
 export default function HeaderDark(){
     return(
         <div className="head" id="head-dark">
-            <img src="%PUBLIC_URL%joeheadshot.jpg" alt="Joe Cuevas" className="headshot" />
+            <img src="./business-cards/joeheadshot.jpg" alt="Joe Cuevas" className="headshot" />
             <h1>Josephcarlos Cuevas</h1>
             <SocialDark />
         </div>
