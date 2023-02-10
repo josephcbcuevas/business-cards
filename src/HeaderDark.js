5d8b4d83bd91f2c0c1568ad80575630fa3cde66a
bundlarjoe <joe.cuevas@bundlar.com>
2023-02-10 13:46:39 -0600

    organized

--- src/HeaderDark.js
+++ src/HeaderDark.js
@@ -3,7 +3,7 @@ import SocialDark from "./SocialDark.js";
 export default function HeaderDark(){
     return(
         <div className="head" id="head-dark">
-            <img src="./images/joeheadshot.jpg" alt="Joe Cuevas" className="headshot" />
+            <img src="./joeheadshot.jpg" alt="Joe Cuevas" className="headshot" />
             <h1>Josephcarlos Cuevas</h1>
             <SocialDark />
         </div>
